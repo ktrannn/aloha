70cf951eddba686a68b50752fb6ee0001d92db89
Kenny <ktran604@hotmail.com>
2017-11-27 11:51:18 -0800

    removed unnecessary files, fixed indentation for css and removed unneeded comments,fixed smooth scrolling again

--- js/script.js
+++ js/script.js
@@ -80,3 +80,8 @@ function validateEmail(sEmail) {
 }
 
 
+$(function() {
+  $("a[href^='#']").not("a[href='#']").click(function() {
+     $("#"+$(this).attr("href").slice(1)+"").focus();
+  });
+});
